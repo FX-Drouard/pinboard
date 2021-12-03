569bf2c994ec6628d8b8f0e96a2b0a3bad8a963c
Sorabush <alexandreyop@hotmail.fr>
2021-12-03 14:20:40 +0100

    ClipEllipse

--- src/pobj/pinboard/document/ClipEllipse.java
+++ src/pobj/pinboard/document/ClipEllipse.java
@@ -1,25 +1,45 @@
 package pobj.pinboard.document;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
-public class ClipEllipse extends AbstractClip {
+public class ClipEllipse extends AbstractClip implements Clip {
+	
+	public ClipEllipse(double left, double top, double right, double bottom, Color color) {
+		setLeft(left);
+		setRight(right);
+		setBottom(bottom);
+		setTop(top);
+		setColor(color);
+	}
 
 	@Override
 	public void draw(GraphicsContext ctx) {
 		// TODO Auto-generated method stub
-		
+		ctx.setFill(getColor());
+		ctx.fillOval(getTop(), getRight(), getLeft(), getBottom());
+		ctx.setStroke(Color.RED);
 	}
 
 	@Override
 	public boolean isSelected(double x, double y) {
 		// TODO Auto-generated method stub
-		return false;
+		double cx= (super.getLeft()+super.getRight())/2;
+		double cy= (super.getTop()+super.getBottom())/2;
+		double rx= (super.getRight()-super.getLeft())/2;
+		double ry= (super.getBottom()-super.getTop())/2;
+		
+		double m1=(x-cx)/rx;
+		double m2=(y-cy)/ry;
+		
+		double res=m1*m1 + m2*m2;
+		return res<=1;
 	}
 
 	@Override
 	public Clip copy() {
 		// TODO Auto-generated method stub
-		return null;
+		return new ClipEllipse(getLeft(),getTop(),getRight(),getBottom(),getColor());
 	}
 
 }
